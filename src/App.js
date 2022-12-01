cc1a144c66f1e5d0afd7becc79154ca9417d5dad
Tommy Han <kattyntommy@gmail.com>
2022-12-01 07:06:48 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./categories.styles.scss";
+import CategoryItem from "./components/category-item/category-item.component";
 const App = () => {
   const categories = [
     {
@@ -30,17 +31,8 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({ id, title, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div
-            className="background-image"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem category={category} />
       ))}
     </div>
   );
