a00ee454192f06cc4dc22af747c4b855f7349d6e
Tommy Han <kattyntommy@gmail.com>
2022-12-02 23:32:00 +0000

    Update 2 files

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,19 @@
+import {
+  signInWithGooglePopup,
+  createUserProfileDocument,
+} from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+    createUserProfileDocument(response);
+  };
+
   return (
     <div>
       <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </div>
   );
 };
