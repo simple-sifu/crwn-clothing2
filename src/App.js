3702bb2836f4f2dda02004c2eba9aa58896007f4
Tommy Han <kattyntommy@gmail.com>
2022-12-02 07:17:13 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route path="home" element={<Home />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,21 @@
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div>Logo</div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
