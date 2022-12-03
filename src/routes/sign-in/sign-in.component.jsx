27803c70b4215b40e21638063e057961ca0cca8f
Tommy Han <kattyntommy@gmail.com>
2022-12-03 08:35:02 +0000

    Update 3 files

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -4,24 +4,27 @@ import {
   createUserDocumentFromAuth,
   signInWithGoogleRedirect,
 } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
+    const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
-
   };
 
   const logGoogleRedirectUser = async () => {
-    const {user} = await signInWithGoogleRedirect();
-    console.log({user});
+    const { user } = await signInWithGoogleRedirect();
+    console.log({ user });
   };
 
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      <button onClick={logGoogleRedirectUser}>Sign in with Google Redirect</button>
+      <button onClick={logGoogleRedirectUser}>
+        Sign in with Google Redirect
+      </button>
+      <SignUpForm />
     </div>
   );
 };
